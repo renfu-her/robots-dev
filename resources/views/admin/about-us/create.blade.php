34541767cc1419d07670e04ad2a47648678034bc
Patrick Her <renfu.her@gmail.com>
2025-02-06 21:48:34 +0800

    Replace EasyMDE with CKEditor in About Us admin views
    - Remove EasyMDE script and stylesheet from create and edit views
    - Include CKEditor partial for consistent rich text editing

--- resources/views/admin/about-us/create.blade.php
+++ resources/views/admin/about-us/create.blade.php
@@ -55,16 +55,5 @@
     </div>
 @endsection
 
-@push('styles')
-    <link rel="stylesheet" href="https://unpkg.com/easymde/dist/easymde.min.css">
-@endpush
-
-@push('scripts')
-    <script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
-    <script>
-        const easyMDE = new EasyMDE({
-            element: document.getElementById('content')
-        });
-    </script>
-@endpush
+@include('admin.partials.ckeditor')
 
